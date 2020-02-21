df973c48f37b3323624a0b4fe99fa7f82bc859b4
Heather Nuffer <zzzuzz@gmail.com>
2020-02-20 17:24:03 -0700

    Displays inputs for logging in to app

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = (props) => {
+    const [userInfo, setUserInfo] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(userInfo);
+    }
+
+    const handleChange = e => {
+        setUserInfo({...userInfo, [e.target.name]:e.target.value});
+    }
+
     return(
-        <h1>Login</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <legend>Login</legend>
+                <hr/>
+                <div>
+                    <label htmlFor='username'>Username / Email</label>
+                    <input name='username' type='text' id='username' required onChange={handleChange}/>
+                </div>
+                <div>
+                    <label htmlFor='password'>Password</label>
+                    <input name='password' type='password' id='password' required onChange={handleChange}/>
+                </div>
+                
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
