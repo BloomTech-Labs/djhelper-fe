85da4c1a7cf7058807c6bcc598b3b40ad19ba28e
Heather Nuffer <zzzuzz@gmail.com>
2020-02-22 17:50:06 -0700

    DJ Interface displays email, website, and phone when set in store

--- src/components/DjInterface.js
+++ src/components/DjInterface.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 const DjInterface = props => {
     const name = useSelector(state => state.userReducer.name);
     const username = useSelector(state => state.userReducer.username);
-    const [phone] = localStorage.getItem('phone') || '';
-    const [website] = localStorage.getItem('website') || '';
-
-    //TODO: Add display of website and phone, especially once we have our own backend.
-    /*
-    {phone && <p>{phone}</p>}
-    {website &&  <p>{website}</p>}
-    */
+    const email = useSelector(state => state.userReducer.email);
+    const phone = useSelector(state => state.userReducer.phone);
+    const website = useSelector(state => state.userReducer.website);
 
     return (
         
-        <div className='dj-interface'>
+        <div>
             <h2>DJ Interface</h2>
-            {name && <h3>{name}</h3>}
-            {username && <p>{username}</p>}
+            <div className='dj-interface'>
+                {name && <h3>{name}</h3>}
+                {username && <p>{username}</p>}
+                {email && <p>{email}</p>}
+                {phone && <p>{phone}</p>}
+                {website &&  <p>{website}</p>}
+            </div>
             
             <p><span>Add Event and Event List coming soon!</span></p>
             
