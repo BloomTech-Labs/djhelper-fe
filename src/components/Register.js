02bb19faa59b2abd1b6181f4ee8a58a82a6136f5
Heather Nuffer <zzzuzz@gmail.com>
2020-02-24 17:27:31 -0700

    Optional registration fields hide until user clicks on button, to not overwhelm the user with too many fields (as per UX research)

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Form, Input} from "reactstrap";
+import {Form, Input, Button} from "reactstrap";
 import { useSelector } from 'react-redux';
 import Loader from 'react-loader-spinner';
 
@@ -15,6 +15,8 @@ const Register = (props) => {
         bio: '',
         profile_pic_url: ''
     });
+
+    const [displayMore, setDisplayMore] = useState(false);
 
     const isRegistering = useSelector(state => state.userReducer.registerUserStart);
 
@@ -62,6 +64,10 @@ const Register = (props) => {
 
     }
 
+    const triggerDisplayMore = () => {
+        setDisplayMore(!displayMore);
+    }
+
     return(
         <div>
             {isRegistering && 
@@ -95,22 +101,26 @@ const Register = (props) => {
                         <label htmlFor='email'>Email</label>
                         <Input name='email' type='email' id='email' onChange={handleChange}/>
                     </div>
-                    <div>
-                        <label htmlFor='website'>Your Website URL</label>
-                        <Input name='website' type='url' id='website' onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label htmlFor='phone'>Phone Number</label>
-                        <Input name='phone' type='phone' id='phone' onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label htmlFor='bio'>Bio</label>
-                        <Input name='bio' type='text' id='bio' onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label htmlFor='profile_pic_url'>Link to Profile Image</label>
-                        <Input name='profile_pic_url' type='text' id='profile_pic_url' onChange={handleChange}/>
-                    </div>
+
+                    <Button onClick={triggerDisplayMore}>{displayMore? 'Hide More Info': 'Add More Info (Optional)' }</Button>
+                    {displayMore && <div className='display-more'>
+                        <div>
+                            <label htmlFor='website'>Your Website URL</label>
+                            <Input name='website' type='url' id='website' onChange={handleChange}/>
+                        </div>
+                        <div>
+                            <label htmlFor='phone'>Phone Number</label>
+                            <Input name='phone' type='phone' id='phone' onChange={handleChange}/>
+                        </div>
+                        <div>
+                            <label htmlFor='bio'>Bio</label>
+                            <Input name='bio' type='text' id='bio' onChange={handleChange}/>
+                        </div>
+                        <div>
+                            <label htmlFor='profile_pic_url'>Link to Profile Image</label>
+                            <Input name='profile_pic_url' type='text' id='profile_pic_url' onChange={handleChange}/>
+                        </div>
+                    </div>}
                     <button type='submit'>Submit</button>
                 </Form>
             }
