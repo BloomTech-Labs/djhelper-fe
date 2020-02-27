9d50dffb73c87f6926bf4da93e5e7ba9466a9ab5
SilverMaiden <h.specials@gmail.com>
2020-02-26 16:13:34 -0800

    Moved PreviewEventDetails to it's own component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -20,18 +20,41 @@ const Dashboard = () => {
         event1: {
             name: "Bill and Grace",
             description: "A traditional, peaceful wedding.",
+            newRequests: {
+                "Mr Blue Sky": "The Electric Light Orchestra",
+                "Eyes": "Rogue Waves",
+                "Don't Stop Me Now": "Queen"
+            }
         },
         event2: {
             name: "Ellie and Mona",
             description: "A more modern, fun wedding.",
+            newRequests: {
+                "Mr Blue Sky": "The Electric Light Orchestra",
+                "Eyes": "Rogue Waves",
+                "Don't Stop Me Now": "Queen"
+            }
+
         },
         event3: {
             name: "Charles and Elizabeth",
-            description: "A senior wedding."
+            description: "A senior wedding.",
+            newRequests: {
+                "Mr Blue Sky": "The Electric Light Orchestra",
+                "Eyes": "Rogue Waves",
+                "Don't Stop Me Now": "Queen"
+            }
+
         },
         event4: {
             name: "Chris and Kat",
-            description: "Very atmospheric and sentimental wedding."
+            description: "Very atmospheric and sentimental wedding.",
+            newRequests: {
+                "Mr Blue Sky": "The Electric Light Orchestra",
+                "Eyes": "Rogue Waves",
+                "Don't Stop Me Now": "Queen"
+            }
+
         },
         active: "",
     })
--- src/components/PreviewEventDetails.js
+++ src/components/PreviewEventDetails.js
@@ -4,31 +4,37 @@ import Songs from './Songs';
 const PreviewEventDetails = (props) => {
     const {data, setData, currentlyActive} = props;
 
+    const whichComponent = () => {
+        if (data.active.length > 1) {
+            return (
+                    <div className="preview-event-details">
+                        <div className="event-description">
+                            <h3> {currentlyActive.name}</h3>
+                            <br />
+                            <h5> Description:</h5>
+                            <br />
+                            {currentlyActive.description}
+                        </div>
+                         <div className="newest-song-requests">
+                            <p> Newest Requests</p>
+                            <Songs />
+                            <Songs />
+                            <Songs />
+                        </div>
+                        <div className="genre-graph">
+                            <p> Genre graph goes here</p>
+                        </div>
+                    </div>
+            )
+        } else {
+            return <p> no event selected</p>;
+        }
+    }
+
     return (
-        <div className="preview-event-details">
-            <div className="event-description">
-                {data.active.length > 1
-                    ?
-                    <p>
-                        <h3> {currentlyActive.name}</h3>
-                        <br />
-                        <h5> Description:</h5>
-                        <br />
-                        {currentlyActive.description}
-                    </p>
-                    : <p> no event selected</p>
-                }
-            </div>
-             <div className="newest-song-requests">
-                <p> Newest Requests</p>
-                <Songs />
-                <Songs />
-                <Songs />
-            </div>
-            <div className="genre-graph">
-                <p> Genre graph goes here</p>
-            </div>
-        </div>
+        <>
+        {whichComponent()}
+        </>
     )
 }
 
