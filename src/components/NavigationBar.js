e488f49a76df480c94d6ad71c18961e57621bbb1
SilverMaiden <h.specials@gmail.com>
2020-02-27 15:50:51 -0800

    Added icons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from '../actions/action';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link, NavLink} from 'react-router-dom';
 
 import {
   Navbar,
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
 } from 'reactstrap';
 
 
@@ -20,26 +21,56 @@ const NavigationBar = (props) => {
       dispatch(logoutUser());
     }
 
+    const navState = () => {
+        if (tokenPresent) {
+            return (
+              <Nav className="navElementsIcons" navbar>
+                <NavItem className="icons">
+                        <NavLink to="/dj/profile" className="icons" data-testid='register-nav'>
+                              <FontAwesomeIcon
+                                icon="user"
+                                size="2x"
+                            />
+                      </NavLink>
+                </NavItem>
+                <NavItem className="icons">
+                      <NavLink id="home" data-testid='register-nav' to="/dj">
+                              <FontAwesomeIcon
+                                icon="home"
+                                size="2x"
+                            />
+                      </NavLink>
+                </NavItem>
+
+                <NavItem>
+                  <NavLink to="/">About</NavLink>
+                        <Nav onClick={handleLogout}>Logout</Nav>
+                </NavItem>
+             </Nav>
+            )
+        } else {
+            return (
+
+              <Nav className=" navElements" navbar>
+                    <NavItem>
+                      <NavLink data-testid='register-nav' to="/register">Register</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink to="/about">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                           <NavLink to="/login">Login</NavLink>
+                    </NavItem>
+
+             </Nav>
+            )
+        }
+    }
+
     return (
         <Navbar className="navBar" data-testid="navBar" dark expand="md">
             <NavbarBrand className="navElements" href="/">DJ Helper</NavbarBrand>
-              <Nav className=" navElements" navbar>
-                <NavItem>
-                  <NavLink  data-testid='register-nav' href="/register">Register</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/Lambda-School-Labs/djhelper-fe">GitHub</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/about">About</NavLink>
-                </NavItem>
-                <NavItem>
-                    {tokenPresent
-                        ? <NavLink  onClick={handleLogout}>Logout</NavLink>
-                        : <NavLink href="/login">Login</NavLink>
-                    }
-                </NavItem>
-         </Nav>
+              {navState()}
         </Navbar>
     )
 }
