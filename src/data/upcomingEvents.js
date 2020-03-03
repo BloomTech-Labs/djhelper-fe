c55be536404e3e77cdd1711447452d6dc66d3f89
Heather Nuffer <zzzuzz@gmail.com>
2020-03-03 15:54:24 -0700

    Adds data file for upcoming events

---
+++ src/data/upcomingEvents.js
@@ -0,0 +1,42 @@
+export const upcomingEvents = {
+    event1: {
+        name: "Bill and Grace",
+        description: "A traditional, peaceful wedding.",
+        newRequests: {
+            "Mr Blue Sky": "The Electric Light Orchestra",
+            "Eyes": "Rogue Waves",
+            "Don't Stop Me Now": "Queen"
+        }
+    },
+    event2: {
+        name: "Ellie and Mona",
+        description: "A more modern, fun wedding.",
+        newRequests: {
+            "Mr Blue Sky": "The Electric Light Orchestra",
+            "Eyes": "Rogue Waves",
+            "Don't Stop Me Now": "Queen"
+        }
+
+    },
+    event3: {
+        name: "Charles and Elizabeth",
+        description: "A senior wedding.",
+        newRequests: {
+            "Mr Blue Sky": "The Electric Light Orchestra",
+            "Eyes": "Rogue Waves",
+            "Don't Stop Me Now": "Queen"
+        }
+
+    },
+    event4: {
+        name: "Chris and Kat",
+        description: "Very atmospheric and sentimental wedding.",
+        newRequests: {
+            "Mr Blue Sky": "The Electric Light Orchestra",
+            "Eyes": "Rogue Waves",
+            "Don't Stop Me Now": "Queen"
+        }
+
+    },
+    active: "",
+}
