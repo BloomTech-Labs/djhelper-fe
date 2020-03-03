d44a9b78af14d82d80bac4bcb72a10a4f920f375
Heather Nuffer <zzzuzz@gmail.com>
2020-03-03 16:25:34 -0700

    Adds event

--- src/data/upcomingEvents.js
+++ src/data/upcomingEvents.js
@@ -45,5 +45,27 @@ export const upcomingEvents = {
         },
         imgUrl: 'https://images.unsplash.com/photo-1494859632785-32abaf5241f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
     },
+    event5: {
+        name: 'Elsa\'s 50th Birthday Gala',
+        description: 'Lots of friends and family to celebrate a very important lady',
+        date: '04/18/2020',
+        newRequests: {
+            'Forever Young': 'Rod Stewart',
+            'Happy Birthday': 'Stevie Wonder',
+            'It\'s My Party': 'Lesley Gore'
+        },
+        imgUrl: 'https://images.unsplash.com/photo-1567768823253-c4fe2bf7143e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    event6: {
+        name: 'Devin\s D&D Event',
+        description: 'All-Night Extreme Game Night',
+        date: '05/20/2020',
+        newRequests: {
+            'Long Ago & Far Away': 'Doug Wood',
+            'The Fair Maiden': 'David Imhof',
+            'All Hail the King': 'Haim Coton'
+        },
+        imgUrl: 'https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
     active: "",
 }
