b80e65ee5bf304334d4e704cd302e46caa84133d
Heather Nuffer <zzzuzz@gmail.com>
2020-03-04 18:50:38 -0700

    Adds dev dependencies, .eslintrc.json, and .prettierrc to set up eslint and prettier
    Co-authored-by: danimal-johnson <danimal.johnson@gmail.com>

--- src/components/DashboardWelcome.js
+++ src/components/DashboardWelcome.js
@@ -1,14 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
-
-const DashboardWelcome = (props) => {
-
-    return (
-        <div className="welcome">
-            <h1 data-testid="header"> Welcome, {props.name}!</h1>
-        </div>
-
-    )
-}
+const DashboardWelcome = props => {
+  return (
+    <div className="welcome">
+      <h1 data-testid="header"> Welcome, {props.name}!</h1>
+    </div>
+  );
+};
 
 export default DashboardWelcome;
