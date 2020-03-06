83ddc2c6b345735fcd21d8645bbbafe34118b2d3
SilverMaiden <h.specials@gmail.com>
2020-03-05 18:05:44 -0800

    Installed react-dotdotdot for multiline truncation on description on dashboard

--- src/components/PreviewEventDetails.js
+++ src/components/PreviewEventDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Songs from './Songs';
+import Dotdotdot from 'react-dotdotdot';
 
 const PreviewEventDetails = props => {
   const { data, setData, currentlyActive } = props;
@@ -8,10 +9,12 @@ const PreviewEventDetails = props => {
     <div className="preview-event-details">
       <div className="event-description">
         <h3 className="bold"> {currentlyActive.name}</h3>
-        <p><b> Event Type:</b> {currentlyActive.eventType}</p>
+        <p><b className="bold"> Event Type:</b> {currentlyActive.eventType}</p>
         <br />
         <p className="bold"> Description:</p>
-        <p className="description">{currentlyActive.description}</p>
+        <Dotdotdot clamp={3}>
+            <p>{currentlyActive.description}</p>
+        </Dotdotdot>
       </div>
       <div className="newest-song-requests">
         <p> Newest Requests</p>
