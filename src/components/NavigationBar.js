e19bfb381770d85d2d6209014829c9353bb5c3b8
Heather Nuffer <zzzuzz@gmail.com>
2020-03-05 17:42:49 -0700

    Fixes cursor for logout in NavigationBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../actions/action';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
-
 import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+import { logoutUser } from '../actions/action';
 
 const NavigationBar = props => {
   const tokenPresent = useSelector(state => state.userReducer.tokenPresent);
@@ -16,6 +15,7 @@ const NavigationBar = props => {
   const handleLogout = () => {
     dispatch(logoutUser());
   };
+
   const selectedIcon = () => {
     console.log(window.location.pathname);
     let url = window.location.pathname;
@@ -35,7 +35,7 @@ const NavigationBar = props => {
           {selectedIcon()}
           <NavItem id="profile">
             <NavLink
-              to="/dj/profile/"
+              to="/dj/profile"
               className={djProfile}
               data-testid="profile"
             >
@@ -43,14 +43,16 @@ const NavigationBar = props => {
             </NavLink>
           </NavItem>
           <NavItem id="home">
-            <NavLink id="home" className={home} data-testid="home" to="/dj/">
+            <NavLink id="home" className={home} data-testid="home" to="/dj">
               <FontAwesomeIcon icon="home" size="2x" />
             </NavLink>
           </NavItem>
 
           <NavItem className="links">
-            <NavLink to="/">About</NavLink>
-            <Nav onClick={handleLogout}>Logout</Nav>
+            <NavLink to="/about">About</NavLink>
+            <Nav className="logout-item" onClick={handleLogout}>
+              Logout
+            </Nav>
           </NavItem>
         </Nav>
       );
