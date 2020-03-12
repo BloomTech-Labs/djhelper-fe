dd199c3915d68ab01204e0755e0684cdaa772644
SilverMaiden <h.specials@gmail.com>
2020-03-12 14:26:01 -0700

    State change made for switching between requests and event playlists

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -8,45 +8,65 @@ import {useDispatch } from 'react-redux';
 
 import { searchForTrack } from '../actions/action';
 
+
+let requestButtonStyle = 'active';
+let playlistButtonStyle = '';
+let addSongsButtonStyle = 'hide';
+let mobilePlaylistView = 'hide';
+let mobileRequestView = '';
+
 const EventPage = (props) => {
     const { name, event_type, description, id, date } = props.location.state.event;
 
-    const [search, setSearch] = useState({
-        buttonText: 'Add Song',
-        searchVisible: false
+    const [switches, setSwitches] = useState({
+        buttonText: 'Add Songs',
+        searchVisible: false,
+        requestButtonActive: true,
+        playlistButtonActive: false,
     });
     const dispatch = useDispatch();
 
     const handleClick = () => {
         let text;
-        if (search.buttonText === 'Add Song') {
+        if (switches.buttonText === 'Add Songs') {
             text='Close Search';
         } else {
-            text='Add Song';
+            text='Add Songs';
             dispatch(searchForTrack(''));
         }
-        setSearch({...search, buttonText: text, searchVisible: !search.searchVisible
+        setSwitches({...switches, buttonText: text, searchVisible: !switches.searchVisible
         });
     }
 
 
-    /*
-     * IN PROGRESS
-    const handleMobileSwitchButtons => () {
-        let
-    }*/
+    const switchToRequests = () => {
+        requestButtonStyle = 'active';
+        playlistButtonStyle = '';
+        addSongsButtonStyle = 'hide';
+        mobilePlaylistView = 'hide';
+        mobileRequestView = '';
+        setSwitches({...switches, requestButtonActive: true, playlistButtonActive: false})
+    }
+
+    const switchToPlaylist = () => {
+        requestButtonStyle = '';
+        playlistButtonStyle = 'active';
+        addSongsButtonStyle = '';
+        mobilePlaylistView = '';
+        mobileRequestView = 'hide';
+        setSwitches({...switches, requestButtonActive: false, playlistButtonActive: true})
+    }
 
     const searchVsPlaylist = () => {
-        if (search.searchVisible) {
+        if (switches.searchVisible) {
             return (
-
-              <div className='playlist'>
+              <div className={`playlist`}>
                 <SongSearch />
                 </div>
             )
         } else {
             return (
-              <div className='playlist'>
+              <div className={`playlist ${mobilePlaylistView}`}>
                     <Songs />
                     <Songs />
                     <Songs />
@@ -84,15 +104,18 @@ const EventPage = (props) => {
             <p>{description}</p>
             <button className='black-button'> Edit </button>
             <h3 id='request-header-text-styling'> Requests </h3>
-            <div className='mobile-switch-buttons'>
-                <button className='playlist-buttons' >
-                    Requests
-                </button>
-                <button className='playlist-buttons'>
-                    Playlist
-                </button>
+            <div className='mobile'>
+                <div className='mobile-switch-buttons'>
+                    <button className={` playlist-buttons ${requestButtonStyle}`}  onClick={() => switchToRequests()}>
+                        Requests
+                    </button>
+                    <button className={`playlist-buttons ${playlistButtonStyle}`}  onClick={() => switchToPlaylist()}>
+                        Playlist
+                    </button>
+                </div>
+                <button className={`bold mobile-add-button ${addSongsButtonStyle}`} onClick={handleClick}>{switches.buttonText}</button>
             </div>
-            <div className='playlist' id='requests'>
+            <div className={`playlist ${mobileRequestView}' id='requests`}>
                 <Songs />
                 <Songs />
                 <Songs />
@@ -104,7 +127,7 @@ const EventPage = (props) => {
         <div className='event-playlist-location'>
             <div className='label'>
                 <h5> Playlist </h5>
-                <p className='bold' onClick={handleClick}>{search.buttonText}</p>
+                <p className='bold' onClick={handleClick}>{switches.buttonText}</p>
             </div>
             {searchVsPlaylist()}
         </div>
