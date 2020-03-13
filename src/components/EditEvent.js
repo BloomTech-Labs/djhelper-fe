24a49debb91670e84121287cd52a8cf52b8ed22b
Heather Nuffer <zzzuzz@gmail.com>
2020-03-13 12:06:29 -0600

    EditEvent page has an input field for name and has initial styling. EventPage edit button toggles EditEvent component display

--- src/components/EditEvent.js
+++ src/components/EditEvent.js
@@ -4,26 +4,41 @@ import { useSelector, useDispatch } from 'react-redux';
 const EditEvent = props => {
   // TODO: Get event data from back end, once it is available (instead of redux store)
   const events = useSelector(state => state.userReducer.events);
-  const [currentEvent] = useState(events[`event${props.event_id}`]);
+  const [currentEvent, setCurrentEvent] = useState(
+    events[`event${props.event_id}`]
+  );
 
-  const handleSubmit = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
     props.setIsEditing(!props.isEditing);
   };
+
+  const handleChanges = e => {
+    setCurrentEvent({ ...currentEvent, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-      <h3 className="bold">{currentEvent.name}</h3>
-      <p>
-        <b className="bold">Event Type:</b> {currentEvent.event_type}
-      </p>
-      <p>
-        <b className="bold">Date: </b>
-        {currentEvent.date}
-      </p>
-      <p className="bold">Description:</p>
-      <p>{currentEvent.description}</p>
-      <button type="button" onClick={handleSubmit}>
-        Submit Changes
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="name"
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChanges}
+          value={currentEvent.name}
+        />
+        <p>
+          <b className="bold">Event Type:</b> {currentEvent.event_type}
+        </p>
+        <p>
+          <b className="bold">Date: </b>
+          {currentEvent.date}
+        </p>
+        <p className="bold">Description:</p>
+        <p>{currentEvent.description}</p>
+        <button type="submit">Submit Changes</button>
+      </form>
     </div>
   );
 };
