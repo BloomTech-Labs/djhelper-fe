805c721d50b396872dc978a8f02966bf86e9979b
Heather Nuffer <zzzuzz@gmail.com>
2020-03-13 14:25:34 -0600

    Adds spinner to EditEvent to display while waiting for data to come from server and adds input active styling

--- src/components/EditEvent.js
+++ src/components/EditEvent.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Loader from 'react-loader-spinner';
 import { editEvent } from '../actions/action';
 
 const EditEvent = props => {
   // TODO: Get event data from back end, once it is available (instead of redux store)
   const events = useSelector(state => state.userReducer.events);
+  const editEventStart = useSelector(state => state.userReducer.editEventStart);
   const [currentEvent, setCurrentEvent] = useState(
     events[`event${props.event_id}`]
   );
@@ -22,46 +24,53 @@ const EditEvent = props => {
 
   return (
     <div className="edit-event-form">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="name"
-          type="text"
-          id="name"
-          name="name"
-          onChange={handleChanges}
-          value={currentEvent.name}
-        />
-        <div className="input-group">
-          <label htmlFor="event_type">Event Type:</label>
+      {editEventStart && (
+        <div className="loader">
+          <Loader type="Audio" color="purple" height={200} width={200} />
+        </div>
+      )}
+      {!editEventStart && (
+        <form onSubmit={handleSubmit}>
           <input
+            className="name"
             type="text"
-            id="event_type"
-            name="event_type"
+            id="name"
+            name="name"
             onChange={handleChanges}
-            value={currentEvent.event_type}
+            value={currentEvent.name}
           />
-        </div>
-        <div className="input-group">
-          <label htmlFor="date">Date:</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            onChange={handleChanges}
-            value={currentEvent.date}
-          />
-        </div>
-        <div className="input-group description">
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            name="description"
-            onChange={handleChanges}
-            value={currentEvent.description}
-          />
-        </div>
-        <button type="submit">Submit Changes</button>
-      </form>
+          <div className="input-group">
+            <label htmlFor="event_type">Event Type:</label>
+            <input
+              type="text"
+              id="event_type"
+              name="event_type"
+              onChange={handleChanges}
+              value={currentEvent.event_type}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="date">Date:</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              onChange={handleChanges}
+              value={currentEvent.date}
+            />
+          </div>
+          <div className="input-group description">
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              name="description"
+              onChange={handleChanges}
+              value={currentEvent.description}
+            />
+          </div>
+          <button type="submit">Submit Changes</button>
+        </form>
+      )}
     </div>
   );
 };
