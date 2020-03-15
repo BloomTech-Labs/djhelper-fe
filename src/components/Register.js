f188a5ad14105460fc3b888f8d97d852a6cceebc
Heather Nuffer <zzzuzz@gmail.com>
2020-03-14 19:03:06 -0600

    Register uses registerUserAction directly, instead of from props originating from App

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Form, Input } from 'reactstrap';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Loader from 'react-loader-spinner';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import NavigationBar from './NavigationBar';
+import { registerUserAction } from '../actions/action';
 
 const Register = props => {
+  const dispatch = useDispatch();
   const [userInfo, setUserInfo] = useState({
     username: '',
     password: '',
@@ -21,15 +23,13 @@ const Register = props => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(userInfo);
-    props.registerUser(userInfo, props.history);
-    setUserInfo({
-      username: '',
-      password: '',
-      repassword: '',
-      name: '',
-      email: ''
-    });
+    const infoNeeded = {
+      username: userInfo.username,
+      password: userInfo.password,
+      name: userInfo.name,
+      email: userInfo.email
+    };
+    dispatch(registerUserAction(infoNeeded, props.history));
   };
 
   const handleChange = e => {
