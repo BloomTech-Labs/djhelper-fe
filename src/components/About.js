ce90507d5d42e8d421e33f870b9d41e2ae591cba
Chawn Christian <chawn.christian@gmail.com>
2020-06-15 23:03:44 -0400

    Changing/adding about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,96 +1,113 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faMusic,
-  faUserCircle,
-  faFile,
-  faEdit,
-  faCheckCircle,
-  faTrophy,
-  faListOl
-} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import NavigationBar from './NavigationBar';
+import Chawn from '../images/Chawn.jpg';
+import Shaya from '../images/Shaya.jpg';
+import Josh from '../images/Josh.jpg';
+import Wangdi from '../images/Wangdi.png';
+import Mohammad from '../images/Mohd.jpg';
+import Evgenii from '../images/Ed.jpg';
+import Maxwell from '../images/Maxwell.jpg';
+import Brian from '../images/Brian.jpg';
+import Dennis from '../images/Dennis.png';
+import Craig from '../images/Craig.jpg';
+import Michael from '../images/Michael.jpg';
+
+
 
 const About = () => {
   return (
     <div className="about-page">
       <NavigationBar />
-      <h1>DJ Helper</h1>
-      <div className="hero">
-        <div className="hero-text">
-          <h2>Know what your audience wants.</h2>
+      <div className="hero"></div>
+      <section className= "aboutTop">
+        <h1>About Us</h1>
+        <div className = "aboutBox">
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+            industry's standard dummy text ever since the 1500s, when an unknown printer
+             took a galley of type and scrambled it to make a type specimen book. 
+             It has survived not only five centuries, but also the leap into electronic
+              typesetting, remaining essentially unchanged. It was popularised in the 
+              1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
         </div>
-      </div>
-      <h1>3 Easy Steps</h1>
-      <div className="steps">
-        <div className="step">
-          <h2>Step 1:</h2>
-          <hr />
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faUserCircle} />
-            </span>
-            <p>Create a DJ account and make pages for your events.</p>
-          </div>
-
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faFile} />
-            </span>
-            <p>Share the event pages with your clients.</p>
-          </div>
+      </section>
+      <h1 className= "title">Web Development Team</h1>
+      <section className="djTeam">
+        
+        <div className ="webteam">
+          <img src ={Shaya}></img>
+          <h3>Shaya</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
         </div>
-
-        <div className="step">
-          <h2>Step 2:</h2>
-          <hr />
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faEdit} />
-            </span>
-            <p>Clients create song requests on the events pages.</p>
-          </div>
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faCheckCircle} />
-            </span>
-            <p>Clients also upvote the requests they want to hear.</p>
-          </div>
+        <div className ="webteam">
+          <img src ={Chawn}></img>
+          <h3>Chawn</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div> <div className ="webteam">
+          <img src ={Mohammad}></img>
+          <h3>Mohammad</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div> <div className ="webteam">
+          <img src ={Josh}></img>
+          <h3>Josh</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div> 
+        <div className ="webteam">
+          <img src ={Wangdi}></img>
+          <h3>Wangdi</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
         </div>
-
-        <div className="step">
-          <h2>Step 3:</h2>
-          <hr />
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faTrophy} />
-            </span>
-            <p>See which songs receive the most votes.</p>
-          </div>
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faMusic} />
-            </span>
-            <p>
-              Get additional recommendations from the app, based on those song
-              requests.
-            </p>
-          </div>
-          <div className="sub-step">
-            <span className="fa-icon">
-              <FontAwesomeIcon icon={faListOl} />
-            </span>
-            <p>Create awesome playlists!</p>
-          </div>
+        </section>
+        <h1 className= "title">Data Science Team</h1>
+        <section className= "djTeam">
+        <div className ="team">
+          <img src ={Evgenii}></img>
+          <h3>Evgenii</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
         </div>
-      </div>
-
-      <div className="get-started">
-        <Link to="/register" data-testid="toRegistration">
-          Let&rsquo;s Get Started
-        </Link>
-      </div>
+        <div className ="team">
+          <img src ={Maxwell}></img>
+          <h3>Maxwell</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div>
+        </section>
+        <h1 className= "title">UX Team</h1>
+        <section className= "djTeam">
+      <div className ="team">
+          <img src ={Brian}></img>
+          <h3>Brian</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div><div className ="team">
+          <img src ={Dennis}></img>
+          <h3>Dennis</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div>
+        </section>
+        <h1 className= "title">IOS Team</h1>
+        <section className="djTeam">
+      <div className ="team">
+          <img src ={Craig}></img>
+          <h3>Craig</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div><div className ="team">
+          <img src ={Michael}></img>
+          <h3>Michael</h3>
+          <p>Lorem Ipsum</p>
+          <p>Lorem Ipsum</p>
+        </div>
+      </section>
+     
     </div>
   );
 };
