7b29c12ccfa28a5d0b8c4e620c2723217394109f
Wangdi Tenzing <44657088+ZIng178@users.noreply.github.com>
2020-07-13 23:25:02 -0400

    Event page almost matches the high def design (#60)
    Need to do some style changes

--- src/components/DashboardWelcome.js
+++ src/components/DashboardWelcome.js
@@ -1,11 +1,19 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import dashboard from '../images/dashboard.png';
 
 const DashboardWelcome = props => {
   return (
-    <div className="welcome">
-      <h1 data-testid="header"> Welcome back, {props.name}!</h1>
+    <>
+   
+    <div>
+    <img  className="dashboardimage"src={dashboard} alt="dashboardimage" />
+    <h1 data-testid="header" className="dashboard-welcome"> Welcome back, {props.name}!</h1>
+
+    <h1  className="dashboard-welcome-event"> Event Playlist</h1>
     </div>
+    
+    </>
   );
 };
 
