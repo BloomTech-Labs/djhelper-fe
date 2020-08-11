8285a7767776f3508c7288097e2ebf5610c62025
Mohd Raza <noorinfotechinc@gmail.com>
2020-08-10 22:01:35 -0500

    fixconflicts

---
+++ src/components/Styles.js
@@ -0,0 +1,74 @@
+export const eventModalStyles = {
+  overlay: { background: 'transparent' },
+  content: {
+    // backgroundColor: '#2997ef',
+    backgroundColor: '#F2F2F2',
+    maxWidth: '520px',
+    maxHeight: '300px',
+    margin: 'auto'
+  }
+};
+
+export const loginModalStyles = {
+  content: {
+    width: '37rem',
+    height: '65rem',
+    margin: 'auto',
+    padding: 0,
+    background: 'transparent',
+    border: 'none'
+  }
+};
+
+export const registerModalStyles = {
+  content: {
+    width: '37rem',
+    height: '65rem',
+    margin: 'auto',
+    padding: 0,
+    background: 'transparent',
+    border: 'none'
+  }
+};
+
+export const trackSearchModalStyles = {
+  content: {
+    width: '80%',
+    margin: '5% auto auto auto',
+    padding: 0,
+    background: 'transparent',
+    border: '1px solid grey'
+  }
+};
+
+export const menuModalStyles = {
+  content: {
+    width: '60%',
+    height: '25%',
+    margin: 'auto',
+    padding: 0,
+    background: '#f2f2f2',
+    border: 'none'
+  }
+};
+
+export const editEventModalStyles = {
+  content: {
+    backgroundColor: '#F2F2F2',
+    maxWidth: '520px',
+    maxHeight: '300px',
+    margin: 'auto',
+    border: '1px solid black'
+  }
+};
+
+export const shareLinkModalStyles = {
+  content: {
+    width: '60%',
+    height: '25%',
+    margin: 'auto',
+    padding: 0,
+    background: '#f2f2f2',
+    border: 'none'
+  }
+};
